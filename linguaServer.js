6bc22c9220c00c01ddca927f3505bf43db5cfc5f
trung2711 <tug57907@temple.edu>
2020-02-24 17:30:47 -0500

    Init node package

--- linguaServer.js
+++ linguaServer.js
@@ -8,7 +8,7 @@ var PORT = 3000;
 
 app.get('/', function(req, res) {
     res.status(200).send('Hello world');
-});np
+});
 
 app.listen(PORT, function() {
     console.log('Server is running on PORT:',PORT);
