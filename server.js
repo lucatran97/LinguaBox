e29197106b49956bc3ef41828335331bf8306608
trung2711 <tug57907@temple.edu>
2020-03-07 17:25:08 -0500

    attempting cloud integration 2

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
-// server.js
-var express = require('express');
-var microsoftTranslate = require('./microsoft.js');
-
-console.log(microsoftTranslate);
-
-var app = express();
-
-var PORT = 3000;
+// @ts-check
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const request = require('request');
 
 async function main() {
-    // on the request to root (localhost:3000/)
-    app.get('/', function (req, res) {
-        res.send('<h1>This server is dedicated to the LinguaBox Project</h1>');
+  // Azure App Service will set process.env.port for you, but we use 3000 in development.
+  const PORT = process.env.PORT || 3000;
+  
+  // Create the express routes
+  let app = express();
+  
+  app.use(express.static('public'));
+  // Change the 404 message modifing the middleware
+  app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
     });
 
-    // On localhost:3000/login
+  app.get('/', function (req, res) {
+    res.send('<h1>This server is dedicated to the LinguaBox Project</h1>');
+    });
+
+  // On localhost:3000/login
     app.get('/login', function (req, res) {
-        //console.log(req.params("id"));
-        res.send('<h1>This is called when an user logs in</h1>');
-    });
+    console.log(req.param("id"));
+    res.send('<h1>This is called when an user logs in</h1>');
+  });
+  
+  // On localhost:3000/chat
+  app.get('/chat', function (req, res) {
+    console.log(req.param("message"));
+    //microsoftTranslate.translate(req.param("message"));
+    //console.log(microsoftTranslate.translate(req.param("message")));
+    //res.send(prepare(microsoftTranslator.translate(req.param("message"))));
+  });
 
-    // On localhost:3000/chat
-    app.get('/chat', function (req, res) {
-        console.log(req.param("message"));
-        microsoftTranslate.translate(req.param("message"));
-        //console.log(microsoftTranslate.translate(req.param("message")));
-        //res.send(prepare(microsoftTranslator.translate(req.param("message"))));
-    });
-
-    // Change the 404 message modifing the middleware
-    app.use(function(req, res, next) {
-        res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-    });
-
-    // Start the server in the port 3000
-    app.listen(PORT, function () {
-        console.log('Example app listening on port 3000.');
-    });
+  // Create the HTTP server.
+  let server = http.createServer(app);
+  server.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+  });
 }
+
+main();
